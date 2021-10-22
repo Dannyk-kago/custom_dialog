ca9ef7d95e0a9c2ff93e42637132e6bccabc52fe
Dannyk_kago <kagokimari@gmail.com>
2021-10-22 12:56:03 +0300

    "Added Dexter Library"

--- app/src/main/java/com/example/customimageselectiondialog/MainActivity.kt
+++ app/src/main/java/com/example/customimageselectiondialog/MainActivity.kt
@@ -6,6 +6,21 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.customimageselectiondialog.databinding.ActivityMainBinding
 import com.example.customimageselectiondialog.databinding.CustomDialogBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import android.Manifest
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.Settings
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.single.PermissionListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myBinding: ActivityMainBinding
@@ -24,14 +39,84 @@ class MainActivity : AppCompatActivity() {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener{
-            Toast.makeText(this,"Camera Clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        if (report.areAllPermissionsGranted()){
+                           val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showDialogForPermissions()
+                }
+
+            }).onSameThread().check()
             dialog.dismiss()
         }
+
+
         binding.tvGallery.setOnClickListener{
-            Toast.makeText(this,"Gallery Clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : PermissionListener{
+                override fun onPermissionGranted(report: PermissionGrantedResponse?) {
+                        Toast.makeText(this@MainActivity,
+                            "Camera permission granted",Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(this@MainActivity,
+                        "Permission Denied to access storage",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest?,
+                    token: PermissionToken?
+                ) {
+                    showDialogForPermissions()
+                }
+
+
+            }).onSameThread().check()
             dialog.dismiss()
         }
 
         dialog.show()
+    }
+
+    private fun showDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off permissions " +
+                "required for this feature. It can be enabled under application settings")
+            .setPositiveButton("GO TO SETTINGS"){
+               _,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+
+            .setNegativeButton("Cancel"){
+                dialog,_->
+                dialog.dismiss()
+            }.show()
+    }
+
+    companion object{
+        private const val CAMERA = 1
     }
 }
